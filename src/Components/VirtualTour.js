12b6b553952b3210d30c3db8828469be2288dade
Poonam-Vijaywargiya <39735459+Poonam-Vijaywargiya@users.noreply.github.com>
2021-08-01 09:47:02 +0530

    Update VirtualTour.js

--- src/Components/VirtualTour.js
+++ src/Components/VirtualTour.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Login from './Login';
-import vr from '../assets/img/VR.gif'
+import movie from '../assets/VirtualVisit.mov'
  function VirtualTour() {
     if (!localStorage.getItem('username')) return <Login />;
 	return (
     <div className="App">
-    <img src={vr}></img>
+    <video src={movie} autoPlay="true"/>
   </div>
 		
 	);
 }
 
 export default VirtualTour;
+
